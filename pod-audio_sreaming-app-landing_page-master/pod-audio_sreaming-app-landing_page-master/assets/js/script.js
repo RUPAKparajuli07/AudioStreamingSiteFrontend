3d90c6e3fec6f78897b1cab0e085c73f6f79d66f
not ∞ <110843279+RUPAKparajuli07@users.noreply.github.com>
2023-12-25 11:36:17 +0545

    Update script.js

--- pod-audio_sreaming-app-landing_page-master/pod-audio_sreaming-app-landing_page-master/assets/js/script.js
+++ pod-audio_sreaming-app-landing_page-master/pod-audio_sreaming-app-landing_page-master/assets/js/script.js
@@ -7,7 +7,7 @@ const elemToggleFunc = function (elem) { elem.classList.toggle("active"); }
 
 // navbar variables
 const navbar = document.querySelector("[data-navbar]");
-const navbarOpenBtn = document.querySelector("[data-nav-open-btn]");
+const navbarOpenBtn = document.querySelector("[data-nav-open-btn]"); 
 const navbarCloseBtn = document.querySelector("[data-nav-close-btn]");
 
 navbarOpenBtn.addEventListener("click", function () {
